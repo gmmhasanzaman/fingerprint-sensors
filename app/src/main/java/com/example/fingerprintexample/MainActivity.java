7ea630028c2d13e3975f24fc7c4c7f78ea307884
HASAN <hasanuzzaman.polock@gmail.com>
2019-12-28 19:50:55 +0600

    initial commit

---
+++ app/src/main/java/com/example/fingerprintexample/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.fingerprintexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.hardware.biometrics.BiometricPrompt;
+import android.os.Bundle;
+import android.os.CancellationSignal;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button authBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        authBtn = findViewById(R.id.authBtn);
+
+        final Executor executor = Executors.newSingleThreadExecutor();
+
+        final BiometricPrompt biometricPrompt = new BiometricPrompt.Builder(this)
+                .setTitle("Fingerprint Authentication")
+                .setSubtitle("Subtitile")
+                .setDescription("Please touch your finger")
+                .setNegativeButton("Cancel", executor, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                }).build();
+
+        authBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // biometricPrompt.authenticate(new CancellationSignal(),executor, new );
+            }
+        });
+    }
+}
