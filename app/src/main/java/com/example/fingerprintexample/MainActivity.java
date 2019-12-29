4fdf941c505b0f90c70023f2e04e1954144f096a
HASAN <hasanuzzaman.polock@gmail.com>
2019-12-29 10:05:01 +0600

    fingerprint work only Android pie and above

--- app/src/main/java/com/example/fingerprintexample/MainActivity.java
+++ app/src/main/java/com/example/fingerprintexample/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.os.CancellationSignal;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -20,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final MainActivity activity = this;
 
         authBtn = findViewById(R.id.authBtn);
 
@@ -39,7 +42,17 @@ public class MainActivity extends AppCompatActivity {
         authBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               // biometricPrompt.authenticate(new CancellationSignal(),executor, new );
+               biometricPrompt.authenticate(new CancellationSignal(), executor, new BiometricPrompt.AuthenticationCallback() {
+                   @Override
+                   public void onAuthenticationSucceeded(BiometricPrompt.AuthenticationResult result) {
+                       activity.runOnUiThread(new Runnable() {
+                           @Override
+                           public void run() {
+                               Toast.makeText(activity, "Success", Toast.LENGTH_SHORT).show();
+                           }
+                       });
+                   }
+               });
             }
         });
     }
