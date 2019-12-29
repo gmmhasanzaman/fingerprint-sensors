f136076f4c8be77fc29cb38a3ba545f1ff7a4e88
HASAN <gmmhasan2017@gmail.com>
2019-12-29 12:21:30 +0600

    fingerprint work only Android pie and above

--- app/src/main/java/com/example/fingerprintexample/MainActivity.java
+++ app/src/main/java/com/example/fingerprintexample/MainActivity.java
@@ -49,8 +49,7 @@ public class MainActivity extends AppCompatActivity {
                            @Override
                            public void run() {
                                Toast.makeText(activity, "Success", Toast.LENGTH_SHORT).show();
-                               //Toast.makeText(activity, "Success", Toast.LENGTH_SHORT).show();
-                               //Toast.makeText(activity, "Success", Toast.LENGTH_SHORT).show();
+
                            }
                        });
                    }
